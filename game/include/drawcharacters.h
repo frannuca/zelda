39994709309c7b75565f4ba8fa191ebb7867c5d1
frannuca <frannuca@gmail.com>
2015-10-10 16:23:01 +0200

    working on the movement of link and the sword

---
+++ game/include/drawcharacters.h
@@ -0,0 +1,20 @@
+//
+// Created by fran on 10.10.15.
+//
+
+#ifndef ANALYTICS_DRAWCHARACTERS_H
+#define ANALYTICS_DRAWCHARACTERS_H
+
+#include <SDL2/SDL.h>
+#include <string>
+
+//class drawcharacters {
+//public:
+//    drawcharacters();
+//    void addCharacter(const std::string& name,const double& width,const double& heigth,const std::string sprites);
+//
+//
+//};
+
+
+#endif //ANALYTICS_DRAWCHARACTERS_H
---
+++ game/src/drawcharacters.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fran on 10.10.15.
+//
+
+#include "drawcharacters.h"
